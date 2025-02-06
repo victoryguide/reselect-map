103f1d664f62dea5c7a5757eb3a455c2c804c426
Eric Palakovich Carr <carreric@gmail.com>
2025-02-06 09:37:19 -0500

    simplify eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,3 @@
-import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import compat from "eslint-plugin-compat";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
-
-export default [
-  ...compat.extends("plugin:compat/recommended"),
-  {
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-      },
-      ecmaVersion: 2021,
-      sourceType: "module",
-    },
-  },
-];
+export default [compat.configs["flat/recommended"]];
